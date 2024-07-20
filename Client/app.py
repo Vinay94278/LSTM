d0607dce588ae01c169e06d5806351991af09dd0
Vinay Aadatiya <adatiyavinayshaileshbhai@gmail.com>
2024-07-21 00:22:56 +0530

    YT Search Model is removed

--- Client/app.py
+++ Client/app.py
@@ -7,17 +7,11 @@ import pickle
 
 # Load the saved model
 model1 = load_model('Model/LSTMm1.keras')
-# model2 = load_model('YT Search Model\LSTMm2.h5')
 
 # Load tokenizer and max sequence length
 with open('Model/tokenizer.pkl', 'rb') as handle:
     tokenizer1 = pickle.load(handle)
 
-# with open('YT Search Model\YTtokenizer.pkl', 'rb') as handle:
-#     tokenizer2 = pickle.load(handle)
-
-  # Adjust as per your training
-# Function to make predictions
 def predict_next_word(model, input_text, tokenizer, max_sequence_len , upto):
     for i in range(upto):
         token_text = tokenizer.texts_to_sequences([input_text])[0]
@@ -28,35 +22,15 @@ def predict_next_word(model, input_text, tokenizer, max_sequence_len , upto):
                 input_text = input_text + " " + word
     return input_text
 
-st.title('Next Word Predictor')
+st.title('Next Word Predictor Using LSTM')
 input_text = st.text_input('Enter the beginning of a sentence:', '')
 upto = st.number_input('Enter the number of future predictions', min_value=1, max_value=20, value=5)
-model_options = {
-    "Professional Email Sentences Model": {
-        "description": "This model is trained on professional email sentences such as 'I hope this email finds you well' and 'Thank you for your prompt response'. It generates phrases and completions suitable for professional communication.",
-        "dataset_link": "https://www.kaggle.com/datasets/shorya22/next-word-predictor-dataset-nlp-task" 
-    },
-    "2024 Trending YouTube India Search Titles Model": {
-        "description": "This model is trained on 2024 trending YouTube India search titles. It generates phrases and completions relevant to popular YouTube search queries in India for the year 2024.",
-        "dataset_link": "https://www.kaggle.com/datasets/rsrishav/youtube-trending-video-dataset"
-    }
-}
+st.write("**Description:** This model is trained on professional email sentences such as 'I hope this email finds you well' and 'Thank you for your prompt response'. It generates phrases and completions suitable for professional communication.")
+st.write("**Dataset Link:** https://www.kaggle.com/datasets/shorya22/next-word-predictor-dataset-nlp-task")
 
-st.title("Model Selection")
-selected_model = st.selectbox("Select a model", options=list(model_options.keys()))
-st.write("### You selected:", selected_model)
-st.write("**Description:**", model_options[selected_model]["description"])
-st.write("**Dataset Link:**", model_options[selected_model]["dataset_link"])
-
-if selected_model == "Professional Email Sentences Model":
-    model = model1
-    tokenizer = tokenizer1
-    max_sequence_len = 21
-else:
-    pass
-    # model = model2
-    # tokenizer = tokenizer2
-    # max_sequence_len = 56
+model = model1
+tokenizer = tokenizer1
+max_sequence_len = 21
 
 if st.button("Predict Next Words"):
     if input_text:
